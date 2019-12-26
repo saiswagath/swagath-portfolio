4d9053f8d279928b02dbe9e1ec6c70d450f5b694
Swagath_Sai_001417567 <kalpatiananthakris.s@husky.neu.edu>
2019-12-26 23:06:13 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/swagathportfolio/precache-manifest.0ceb3e2fdc01348cd0ba444831655b12.js"
+  "/swagath-portfolio/precache-manifest.4d38d38cd97a45e47770d07758fa8f29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swagathportfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/swagath-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
